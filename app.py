f2ef2562d5a1899102cc95407da0845bf5e3be87
Andrei Tuta <tuta.andrei96@gmail.com>
2023-08-12 19:35:38 +0100

    Update as per newest geckodrive

--- app.py
+++ app.py
@@ -9,12 +9,13 @@ import urllib
 import requests
 import sys
 
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
 from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.common.keys import Keys
+
+from selenium.webdriver import FirefoxOptions
+
 
 def randdelay(a, b):
     time.sleep(random.uniform(a, b))
@@ -23,21 +24,32 @@ def randdelay(a, b):
 def u_to_s(uni):
     return unicodedata.normalize('NFKD', uni).encode('ascii', 'ignore')
 
-class PinterestDownloader():
+class PinterestDownloader:
+    """Class to download Pinterest images."""
+    
     def __init__(self):
-        print('Initialised image downloader')
+        print('Initialized image downloader')
 
-    def download(self, image_url, query_param, image_name):
+    def download(self, image_url: str, query_param: str) -> None:
+        """Downloads an image from the given URL.
+
+        Args:
+            image_url (str): The URL of the image to be downloaded.
+            query_param (str): The query parameter used for organizing the downloaded images.
+        """
         print(f'Downloading image from {image_url}')
         img_data = requests.get(image_url).content
-        with open(f'results/{query_param}/{image_name}.jpg', 'wb') as handler:
+        image_url = str(image_url)[:-1].split('/')[-1]
+        with open(f'results/{query_param}/{image_url}', 'wb') as handler:
             handler.write(img_data)
 
-class PinterestHelper(object):
-    def __init__(self, url, threshold=10):
-        options = Options()
-        self.browser = webdriver.Firefox(executable_path='/usr/bin/geckodriver', options=options)
-        # self.login(email, pw)
+class PinterestHelper:
+    """Class to interact with Pinterest using Selenium."""
+
+    def __init__(self, url: str, threshold: int = 10):
+        opts = FirefoxOptions()
+        opts.add_argument("--headless")
+        self.browser = webdriver.Firefox(executable_path='./bin/geckodriver', options=opts)
         self.images = []
         tries = 0
         try:
@@ -46,11 +58,20 @@ class PinterestHelper(object):
         except (socket.error, socket.timeout):
             pass
 
-    def close(self):
-        """ Closes the browser """
+    def close(self) -> None:
+        """Closes the browser."""
         self.browser.close()
 
-    def process_images(self,tries=0, threshold=500):
+    def process_images(self, tries: int = 0, threshold: int = 500) -> list[str]:
+        """Processes images found on the Pinterest page.
+
+        Args:
+            tries (int): The number of attempts made for processing.
+            threshold (int): Maximum number of processing attempts.
+
+        Returns:
+            List of image URLs.
+        """
         results = []
         while threshold > 0:
             print(f'Processing {tries}')
@@ -66,17 +87,23 @@ class PinterestHelper(object):
                             src = src.replace("/236x/", "/736x/")
                             src = src.replace("/474x/", "/736x/")
                             results.append(u_to_s(src))
-                body = self.browser.find_element(By.XPATH,'/html/body')
+                body = self.browser.find_element(By.XPATH, '/html/body')
                 body.send_keys(Keys.PAGE_DOWN)
                 randdelay(0, 1)
-                tries+=1
+                tries += 1
             except StaleElementReferenceException:
-                tries+=1
+                tries += 1
         print(f'Processed {len(results)}')
         return results
 
-    def write_results(self, query_param, images):
-        print(f'Saving {len(images)} urls to text file')
+    def write_results(self, query_param: str, images: list[str]) -> None:
+        """Writes image URLs to a text file and downloads images.
+
+        Args:
+            query_param (str): The query parameter used for organizing the downloaded images.
+            images (list): List of image URLs to be written and downloaded.
+        """
+        print(f'Saving {len(images)} URLs to text file')
         if not os.path.exists(f'results/{query_param}'):
             print(f'Creating results/{query_param}')
             os.makedirs(f'results/{query_param}')
@@ -86,13 +113,13 @@ class PinterestHelper(object):
                 for file in files:
                     os.remove(os.path.join(root, file))
             print(f'Cleaned folder of previous results')
-        # save results in a file
+        
         with open(f'results/{query_param}/'+query_param.replace(" ", "") + "_pins.txt", "w") as file:
             file.write('\n'.join([i.decode('UTF-8') for i in images]))
-        # then download images to file
+        
         self.downloader = PinterestDownloader()
         for image in images:
-            self.downloader.download(image, query_param, f'{query_param}-{images.index(image)}')
+            self.downloader.download(image, query_param)
         print(f'Saved {len(images)} images')
 
 def main():
@@ -101,7 +128,7 @@ def main():
         term = args[1]
         limit = 10
         try:
-            limit = args[2]
+            limit = int(args[2])
         except IndexError:
             print(f'No limit passed. Defaulting to 10')
         ph = PinterestHelper('http://pinterest.com/search/pins/?q=' + urllib.parse.quote(term), limit)
@@ -109,8 +136,6 @@ def main():
         ph.close()
     else:
         print(f'Parameters passed are wrong. Please check')
-
-
 
 if __name__ == '__main__':
     main()
--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 rm -rf ~/tmp/some-project
 mkdir -p ~/tmp/some-project
-export TMPDIR=/home/at/tmp/some-project
+export TMPDIR=~/tmp/some-project
 rm -rf results/*
 
-python app.py **term** 
+python app.py "books infographic" 2
